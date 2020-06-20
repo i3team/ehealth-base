c7ac37b8b8e2076aba4f5446037c3b75dc7c1801
i3team <61685630+i3team@users.noreply.github.com>
2020-06-20 18:27:07 +0700

    Add files via upload

---
+++ BaseTableButton.jsx
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import PropTypes from "prop-types";
+import BaseButton from './BaseButton';
+
+class BaseTableButton extends BaseButton {
+    constructor(props) {
+        super(props);
+    }
+    static propTypes = {
+        selectedItems: PropTypes.array.isRequired
+    }
+    isApplicable() {
+        return this.props.selectedItems.some(item => this.isItemApplicable(item));
+    }
+    actionName() {
+        throw 'Not implemented actionName';
+    }
+    rowUnitName(){
+        return 'hàng';
+    }
+    text() {
+        const { selectedItems } = this.props;
+        let count = selectedItems.filter(item => this.isItemApplicable(item)).length
+        return this.actionName() + ` ${count} ${this.rowUnitName()}`;
+    }
+    isItemApplicable(item) {
+        throw 'Not implemented `isItemApplicable`';
+    }
+}
+
+export default BaseTableButton;
