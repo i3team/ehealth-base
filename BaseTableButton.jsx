1b9a3390a07904eeb5e26b8e2654e34ffb87e597
i3team <61685630+i3team@users.noreply.github.com>
2020-08-11 13:41:53 +0700

    Update BaseTableButton.jsx

--- BaseTableButton.jsx
+++ BaseTableButton.jsx
@@ -1,31 +1,40 @@
 import React, { Fragment } from 'react';
 import PropTypes from "prop-types";
 import BaseButton from './BaseButton';
+import { readKey } from '../../general/helper';
+
 
 class BaseTableButton extends BaseButton {
     constructor(props) {
         super(props);
     }
     static propTypes = {
-        selectedItems: PropTypes.array.isRequired,
+        selectedItems: PropTypes.array,
         callback: PropTypes.func,
+        closeDrawer: PropTypes.func,
+    }
+    getSelectedItems(){
+        return this.props.selectedItems.map(i => ({
+            key: readKey(i.key, i.headerLevel, i.footerLevel),
+            dataType: i.dataType
+        }));
     }
     isApplicable() {
         return this.props.selectedItems.some(item => this.isItemApplicable(item));
     }
+    isItemApplicable(item) {
+        throw 'Not implemented `isItemApplicable`';
+    }
     actionName() {
         throw 'Not implemented actionName';
     }
-    rowUnitName(){
+    rowUnitName() {
         return 'hàng';
     }
     text() {
         const { selectedItems } = this.props;
         let count = selectedItems.filter(item => this.isItemApplicable(item)).length
         return this.actionName() + ` ${count} ${this.rowUnitName()}`;
-    }
-    isItemApplicable(item) {
-        throw 'Not implemented `isItemApplicable`';
     }
 }
 
